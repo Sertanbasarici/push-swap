d64579bee5666f7168f37728cb7247ab9663ef96
Sertan Goktug Basarici <sebasari@k1m02s03.42istanbul.com.tr>
2024-02-29 17:03:11 +0300

    update

--- push_swap_control.c
+++ push_swap_control.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap_util.c                                   :+:      :+:    :+:   */
+/*   push_swap_control.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sebasari <sebasari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 18:39:22 by sebasari          #+#    #+#             */
-/*   Updated: 2024/02/17 19:04:54 by sebasari         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:55:25 by sebasari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_swap(t_list **node)
 {
-	if (!node || !(*node))
+	if (!node || !(*node) || (ft_lstsize(*node) == 1))
 		return ;
-	t_list  *tmp;
-	t_list  *tmp1;
-	t_list  *tmp2;
+	t_list	*tmp;
+	t_list	*tmp1;
+	t_list	*tmp2;
 	tmp2 = (*node) -> next -> next;
 	tmp1 = (*node) -> next;
 	tmp = (*node);
@@ -74,14 +74,17 @@ void	pb(t_list **b, t_list **a)
 
 void	ft_rotate(t_list **node)
 {
-	if (!node || !(*node))
+	if (!node || !(*node) || (ft_lstsize(*node) == 1))
 		return ;
 	t_list	*tmp;
+	t_list	*tmp1;
 	tmp = (*node);
+	tmp1 = (*node) -> next;
 	while ((*node) -> next != NULL)
 		(*node) = (*node) -> next;
 	tmp -> next = (*node) -> next;
 	(*node) -> next = tmp;
+	(*node) = tmp1;
 }
 
 void	ra(t_list **a)
@@ -111,12 +114,41 @@ void	rr(t_list **a, t_list **b)
 
 void	ft_reverse_rotate(t_list **node)
 {
-	if (!node || !(*node))
+	if (!node || !(*node) || (ft_lstsize(*node) == 1))
 		return ;
-	t_list *tmp;
+	t_list	*tmp;
+	t_list	*tmp1;
 	tmp = (*node);
 	while ((*node) -> next -> next != NULL)
 		(*node) = (*node) -> next;
 	(*node) -> next -> next = tmp;
+	tmp1 = (*node);
+	tmp1 = tmp1 -> next; 
 	(*node) -> next = NULL;
+	(*node) = tmp1;
+}
+
+void	rra(t_list	**a)
+{
+	if (!(a) || !(*a))
+		return ;
+	ft_reverse_rotate(a);
+	ft_printf("rra\n");
+}
+
+void	rrb(t_list	**b)
+{
+	if (!(b) || !(*b))
+		return ;
+	ft_reverse_rotate(b);
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_list **a, t_list **b)
+{
+	if (!(a) || !(*a) || !(b) || !(*b))
+		return ;
+	ft_reverse_rotate(a);
+	ft_reverse_rotate(b);
+	ft_printf("rrr\n");
 }
